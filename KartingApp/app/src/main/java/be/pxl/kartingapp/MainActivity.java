2f4d14dfe62fa0dfe9745ff1755a1fc4524551aa
MatthiasVDB <matthias.vandenbergh@student.pxl.be>
2018-10-16 16:18:28 +0200

    Added database functionality

--- KartingApp/app/src/main/java/be/pxl/kartingapp/MainActivity.java
+++ KartingApp/app/src/main/java/be/pxl/kartingapp/MainActivity.java
@@ -2,15 +2,23 @@ package be.pxl.kartingapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.sql.Date;
+
+import be.pxl.kartingapp.data.KartingContract;
+import be.pxl.kartingapp.data.KartingDbHelper;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button bCreateNewSession;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        KartingDbHelper dbHelper = new KartingDbHelper(this);
+
+        db = dbHelper.getWritableDatabase();
+        //Uncomment when you want to add dummy data
+        //System.out.println(dbHelper.insertCircuit("dummyKarting", "dummyAddress"));
+        //System.out.println(dbHelper.insertSession("FULL", new Date(0), 1));
+        //System.out.println(dbHelper.insertLap("0:43.123", 1));
+    }
+
+    private Cursor getAllCircuits() {
+        return db.query(
+                KartingContract.CircuitEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
     }
 
 }
